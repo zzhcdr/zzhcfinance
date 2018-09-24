a15a3a2a4a298d4f0f07798a386deda910fa0205
lvchenghui <fengxiangpiao@hotmail.com>
2018-09-24 15:45:44 +0800

    修改主页面

--- pages/report/report.js
+++ pages/report/report.js
@@ -115,5 +115,27 @@ Page({
       wx.navigateTo({
         url: '../voucher/voucheradd',
       })
+  },
+
+  //获取当前滑块的index
+  bindchange: function (e) {
+    const that = this;
+    that.setData({
+      currentData: e.detail.current
+    })
+  },
+  //点击切换，滑块index赋值
+  checkCurrent: function (e) {
+    const that = this;
+
+    if (that.data.currentData === e.target.dataset.current) {
+      return false;
+    } else {
+
+      that.setData({
+        currentData: e.target.dataset.current
+      })
+    }
   }
+
 })
